cb9cb51fe9e10219670a6ff388f40281d0a81db0
leirdan <uouleirdan@gmail.com>
2025-01-08 23:41:30 -0300

    temp

--- red_black_tree/main.cpp
+++ red_black_tree/main.cpp
@@ -3,57 +3,18 @@
 
 #include <iostream>
 
-
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
     Tree *tree = new Tree();
 
-    std::cout << "Insira a quantidade de elementos a serem inseridos na árvore: ";
-    int n;
-    std::cin >> n;
-    std::cout << std::endl;
-    for (int i = 0; i < n; i++) {
-        std::cout << "Insira o elemento a ser inserido na árvore: ";
-        int key;
-        std::cin >> key;
-        std::cout << std::endl;
-        tree->insert(key);
-        tree->print();
-        std::cout << std::endl;
+    int L[] = {15, 18, 20, 35, 32, 38, 30, 40, 32, 45, 48, 52, 60, 50};
+
+    for (int i = 0; i < 14; i++)
+    {
+        tree->insert(L[i]);
     }
 
-    std::cout << "Árvore pós inserções: " << std::endl;
-    std::cout << std::endl;
+    std::cout << "Elementos inseridos. Imprimindo árvore...\n";
     tree->print();
-    
-    std::cout << std::endl;
-    std::cout << "Insira um elemento a ser encontrado na árvore: ";
-    int key;
-    std::cin >> key;
-    Node *node = tree->search(key);
-    if (node != nullptr) {
-        std::cout << "Encontrado: " << node->key << " nivel: " << node->depth << " cor: " << (node->color ? "RED" : "BLACK") << std::endl;
-    } else {
-        std::cout << "Não Encontrado." << std::endl;
-    }
-    std::cout << std::endl;
-
-    std::cout << "Insira o número de elementos a serem removidos: ";
-    std::cin >> n;
-    std::cout << std::endl;
-    for (int i = 0; i < n; i++) {
-        std::cout << "Insira um elemento para remover da árvore: ";
-        int key;
-        std::cin >> key;
-        std::cout << std::endl;
-        tree->remove(key);
-        tree->print();
-        std::cout << std::endl;
-    }
-
-    std::cout << "Árvore pós remoções: " << std::endl;
-    std::cout << std::endl;
-    tree->print();
-
-    delete tree;
-    return EXIT_SUCCESS;
+    std::cout << "Finalizado.\n";
 }
